012f030c1ab13db338d0649571b2bdb16c1a92b9
Ali Mahmmoud <127209252+Ali2653766@users.noreply.github.com>
2025-04-13 20:45:36 +0000

    eedited something

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let stars = document.getElementById('stars');
+    let moon = document.getElementById('moon');
+    let mountain1 = document.getElementById('mountains1');
+    let mountain2 = document.getElementById('mountains2');
+    let river = document.getElementById('river');
+    let boat = document.getElementById('boat');
+
+    window.onscroll = function() {
+        let value = scrollY;
+        stars.style.left = value + 'px';
+        moon.style.top = value * 3 + 'px';
+        mountain1.style.top = value * 1.5 + 'px';
+        mountain2.style.top = value * 1.2 + 'px';
+        river.style.top = value + 'px';
+        boat.style.left = value * 3 + 'px';
+        boat.style.top = value * 1.5 + 'px';
+    }
+});
