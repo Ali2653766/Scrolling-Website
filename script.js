05ab4cafae8f77967e9cbf174231b8f886331105
Ali Mahmmoud <127209252+Ali2653766@users.noreply.github.com>
2025-04-13 20:58:48 +0000

    Working on bug:something is wrong with images

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let moon = document.getElementById('moon');
     let mountain1 = document.getElementById('mountains1');
     let mountain2 = document.getElementById('mountains2');
+    let mountain3 = document.getElementById('mountains3');
     let river = document.getElementById('river');
     let boat = document.getElementById('boat');
 
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         moon.style.top = value * 3 + 'px';
         mountain1.style.top = value * 1.5 + 'px';
         mountain2.style.top = value * 1.2 + 'px';
+        mountain3.style.top = value + 'px';
         river.style.top = value + 'px';
         boat.style.left = value * 3 + 'px';
         boat.style.top = value * 1.5 + 'px';
